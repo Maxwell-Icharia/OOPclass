7b991262d2af77b027c909904a61e4db9e832d7b
Maxwell-icharia <maxwellicharia@gmail.com>
2017-01-20 01:11:52 +0300

    Updated code enabled appropriate OOP functionality

--- myclass.py
+++ myclass.py
@@ -1,22 +1,23 @@
 class Myclass(object):
 	"""docstring for Myclass"""
-	def __init__(self, arg):
-		super(Myclass, self).__init__()
-		self.arg = arg
-		id = 0
-		name = "noname"
-		colour = ''
-	def Main():
-		me = Myclass()
-		me.name = 'Max'
-		me.id = 4
-		me.colour = 'Brown'
+	def __init__(self, id, name, age):
+		self.id = id
+		self.name = name
+		self.age = age
+	def nname(self):
+		if name != str:
+			return 'Invalid name, try again!'
+	def age(self):
+		if (age != int) & (age < 18):
+			return 'Invalid age, try again!'
+	def idd(self):
+		if id in Myclass.idd:
+			return 'Already exists'
 
-		friend = Myclass()
-		friend.name = "Steve"
-		friend.id = 6
-		friend.colour = 'Grey'
-
-
-if __name__=='__main__':
-	Main()
+class Display(Myclass):
+	def get_name(self):
+		return 'Name: ', name
+	def get_age(self):
+		return 'Age: ', age
+	def get_id():
+		return 'ID number: ', id
